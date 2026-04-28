a19d1ac6260e819acdbb668273191ce76fdfedf3
killsittxx <kissmmx65@gmail.com>
2026-04-28 19:52:31 +0300

    api_client.py

--- api_client.py
+++ api_client.py
@@ -2,9 +2,6 @@ import time
 
 def check_virus_total(value, api_key=None):
     if not api_key:
-        # Mock-данные (имитация работы API)
+       
         return "Clean (Mock)" if len(value) % 2 == 0 else "Malicious (Mock)"
-    
-    # Здесь могла бы быть логика реального запроса к VirusTotal
-    # Но для теста оставим имитацию
     return "Checked via API"
